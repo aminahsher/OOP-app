2469c1f506c93ee8e4ef79a7de04b2ad925e8536
Aminah Ali <aania2009@gmail.com>
2024-05-20 10:39:55 -0700

    updated index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 console.log("hello world");
+let x = 5;
+let y = 10;
+function add(x, y) {
+    return x + y;
+}
+add(5, 10);
 export {};
--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
 console.log("hello world")
+let x = 5
+let y = 10
+function add(x:number, y:number){
+    return x+y
+}
+add(5,10)
